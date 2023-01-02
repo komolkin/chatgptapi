91131b872e5405c8049d6484b693d2b8fe17ccec
Ilya Komolkin <komolkin@gmail.com>
2023-01-02 17:50:36 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// a express server, which will handle api requests coming in and respond back with a json object, it will use a body parser as well as cors
+
+const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = OpenAI;
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+const configuration = new Configuration({
+  organization: "org-9WevQbvggItOcNJtMQyQhS4L",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Pretend you are Kevin Durant.
+    Person: ${message}?
+    Kevin:`,
+    max_tokens: 100,
+    temperature: 0,
+  });
+  console.log(response.data);
+  if (response.data.choices[0].text) {
+    res.json({ message: response.data.choices[0].text });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// create a react component that inputs a textarea message then performs a fetch request to localhost:3001 gets back a response as a data.message and that message in a box below
+
+import React, { useState } from "react";
+import "inter-ui/inter.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form } from "react-bootstrap";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3001/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    })
+      .then((res) => res.json())
+      .then((data) => setResponse(data.message));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "400px", margin: "auto auto" }}>
+      <div className="response">
+        {response && (
+          <div style={{ marginTop: "8rem" }}>
+            <div className="card" style={{ marginBottom: "1rem" }}>
+              <div className="card-body">
+                <b>Me:</b> {message}
+              </div>
+            </div>
+            <div className="card" style={{ marginBottom: "1rem" }}>
+              <div className="card-body">
+                <b>KD:</b> {response}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <Form
+        onSubmit={handleSubmit}
+        style={{ display: "flex", flexFlow: "row" }}
+      >
+        <Form.Control
+          value={message}
+          placeholder="Ask Kevin Durant any question"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button variant="dark" type="submit" style={{ marginLeft: "10px" }}>
+          Send
+        </Button>
+      </Form>
     </div>
   );
 }
